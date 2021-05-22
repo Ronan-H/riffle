962321a95447758df1714c4444123fbcf083d531
Ronan-H <g00342393@gmit.ie>
2021-05-22 22:02:13 +0100

    Move page headers into the navbar

--- riffle-client/src/app/navbar/navbar.component.ts
+++ riffle-client/src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavbarService } from './navbar.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public navbarService: NavbarService,
+  ) {}
 
   ngOnInit(): void {
   }
