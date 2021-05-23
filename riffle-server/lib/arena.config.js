a0ab6038a16b9024c263e9f371ebde880b22f251
Ronan-H <g00342393@gmit.ie>
2021-05-23 21:13:45 +0100

    Build deploy - 23 May 2021 21:13:45

--- riffle-server/lib/arena.config.js
+++ riffle-server/lib/arena.config.js
@@ -9,6 +9,13 @@ const express_1 = __importDefault(require("express"));
 const path_1 = __importDefault(require("path"));
 const RiffleRoom_1 = require("./RiffleRoom");
 const compression = require('compression');
+// Ref: https://stackoverflow.com/a/31144924
+function requireHTTPS(req, res, next) {
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
 exports.default = arena_1.default({
     getId: () => "Riffle",
     initializeGameServer: (gameServer) => {
@@ -20,18 +27,9 @@ exports.default = arena_1.default({
     initializeExpress: (app) => {
         app
             .use(compression())
+            .use(requireHTTPS)
             .use(express_1.default.static(path_1.default.join(__dirname, 'static')))
             .use((req, res) => res.sendFile('index.html', { root: path_1.default.join(__dirname, 'static') }));
-        // app.use('/', (req, res) => 
-        //     res.sendFile('index.html', { root: path.join(__dirname, 'static') })
-        // );
-        // app.use('/', serveIndex(path.join(__dirname, "static"), {'icons': true}))
-        // app.use('/assets', express.static(path.join(__dirname, 'static', 'assets')));pp.get('/', function(req, res){
-        //     res.sendFile('default.html', { root: __dirname + "/relative_path_of_file" } );
-        // });
-        // app.use('*', express.static(path.join(__dirname, "static")));
-        // https://docs.colyseus.io/tools/monitor/
-        // app.use("/colyseus", monitor());
     },
     beforeListen: () => {
         // Before before gameServer.listen() is called.
